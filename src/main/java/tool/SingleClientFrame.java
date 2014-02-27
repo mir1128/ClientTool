576a7928b0d39cdd92c5b9c57cd7cc9eb48854b8
Jie Liu <jie.liu@rea-group.com>
2014-02-27 13:20:58 +0800

    index id

--- src/main/java/tool/SingleClientFrame.java
+++ src/main/java/tool/SingleClientFrame.java
@@ -15,6 +15,7 @@ public class SingleClientFrame extends JPanel {
     private JTextArea messageAreas;
     private JTextArea sendArea;
     private JComboBox selectSendMessage;
+    private JButton receiveButton;
 
     private SimpleSocketClient clients;
 
@@ -22,12 +23,12 @@ public class SingleClientFrame extends JPanel {
 
     private void initMessageTemplateMap(){
         messageTemplateMap.put("001 : playerID", "{\"MsgID\" : \"001\",\"Msg\": \"1001\"}");
-        messageTemplateMap.put("201 : T Move", "{\"MsgID\" :201,\"Msg\" : \"(2,3)\"}");
-        messageTemplateMap.put("301 : P Type", "{ \"MsgID\" :301,\"Msg\": \"walk\"}");
-        messageTemplateMap.put("302 : P Deduce","{\"MsgID\": 302,\"Msg\": \"(1,1)100 (2,2)80 (1,2)70 (12,3)0\"}");
-        messageTemplateMap.put("303 : P Proposal","{\"MsgID\": 303,\"Msg\" :{\"playerid1\" : \"(10,10)\",\"playerid2\" : \"(10,10)\",\"playerid3\" : \"(10,10)\",\"playerid4\" : \"(10,10)\",\"playerid5\" : \"(10,10)\",}}");
-        messageTemplateMap.put("304 : P Vote","{\"MsgID\": 304,\"Msg\": [\"playerid1\",\"playerid2\",\"playerid3\",\"playerid4\",\"playerid5\"]}");
-        messageTemplateMap.put("305 : P Move","{\"MsgID\":305,\"Msg\":(2,3)}");
+        messageTemplateMap.put("201 : T Move", "{\"MsgID\" :\"201\" ,\"Msg\" : \"(2,3)\"}");
+        messageTemplateMap.put("301 : P Type", "{ \"MsgID\" :\"301\" ,\"Msg\": \"walk\"}");
+        messageTemplateMap.put("302 : P Deduce","{\"MsgID\": \"302\" ,\"Msg\": \"(1,1)100 (2,2)80 (1,2)70 (12,3)0\"}");
+        messageTemplateMap.put("303 : P Proposal","{\"MsgID\": \"303\" ,\"Msg\" :{\"playerid1\" : \"(10,10)\",\"playerid2\" : \"(10,10)\",\"playerid3\" : \"(10,10)\",\"playerid4\" : \"(10,10)\",\"playerid5\" : \"(10,10)\",}}");
+        messageTemplateMap.put("304 : P Vote","{\"MsgID\": \"304\" ,\"Msg\": [\"playerid1\",\"playerid2\",\"playerid3\",\"playerid4\",\"playerid5\"]}");
+        messageTemplateMap.put("305 : P Move","{\"MsgID\":\"305\" ,\"Msg\":(2,3)}");
     }
 
 
@@ -39,6 +40,7 @@ public class SingleClientFrame extends JPanel {
         sendButton = new JButton("p"+id+" 发送");
         connectButton = new JButton("p"+id+" 链接");
         disconnectButton = new JButton("p"+id+" 断开");
+        receiveButton = new JButton("p"+id+"接收");
         messageAreas = new JTextArea(28, 17);
         sendArea = new JTextArea(13, 17);
         selectSendMessage = new JComboBox();
@@ -47,6 +49,7 @@ public class SingleClientFrame extends JPanel {
         connectButton.addActionListener(new ConnectButtonListener());
         disconnectButton.addActionListener(new DisconnectButtonListener());
         selectSendMessage.addActionListener(new ComboBoxSelectActionListener());
+        receiveButton.addActionListener(new ReceiveButtonListener());
 
         for (String key : messageTemplateMap.keySet()){
             selectSendMessage.addItem(key);
@@ -57,7 +60,19 @@ public class SingleClientFrame extends JPanel {
         add( connectButton);
         add( disconnectButton);
         add( sendButton);
+        add(receiveButton);
         add(selectSendMessage);
+    }
+
+    class ReceiveButtonListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                messageAreas.append(clients.readMsg());
+            } catch (IOException e1) {
+                e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
     }
 
     class ConnectButtonListener implements ActionListener {
@@ -90,11 +105,11 @@ public class SingleClientFrame extends JPanel {
             String message = null;
             messageAreas.append(sendArea.getText());
             clients.sendMsg(sendArea.getText());
-            try {
-                messageAreas.append(clients.readMsg());
-            } catch (IOException e1) {
-                e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }
+//            try {
+//                messageAreas.append(clients.readMsg());
+//            } catch (IOException e1) {
+//                e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//            }
             sendArea.setText("");
         }
     }
@@ -105,7 +120,12 @@ public class SingleClientFrame extends JPanel {
         public void actionPerformed(ActionEvent e) {
             String selectItem = (String)((JComboBox)e.getSource()).getSelectedItem();
 
-            sendArea.setText(messageTemplateMap.get(selectItem));
+            if (selectItem.equals("001 : playerID")){
+                sendArea.setText(messageTemplateMap.get(selectItem).replace("1001", "100"+(id+1)));
+            }
+            else {
+                sendArea.setText(messageTemplateMap.get(selectItem));
+            }
         }
     }
 
